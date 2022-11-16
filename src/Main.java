21cfecc0c2333cf84391e7e755786ab6ba9d87ec
ilyas Arikan <ilyasarikan@gmail.com>
2022-11-16 19:16:21 +0300

    ucgen alan

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
         int a, b, c;
         double u, alan;
         Scanner inp = new Scanner(System.in);
-        System.out.println("Kenar uzunlugunu giriniz: ");
+        System.out.println("Kenar uzunlugunu giriniz:  ");
         a = inp.nextInt();
-        System.out.println("Kenar uzunlugunu giriniz: ");
+        System.out.println("Kenar uzunlugunu giriniz:  ");
         b = inp.nextInt();
-        System.out.println("Kenar uzunlugunu giriniz: ");
+        System.out.println("Kenar uzunlugunu giriniz:  ");
         c = inp.nextInt();
         u = (a+b+c) / 2;
-        System.out.println("Ucgeninizin çevresi: " + u);
+        System.out.println("Ucgeninizin çevresi:  " + u);
         alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
-        System.out.print("Dairenizini alanı :" + alan);
+        System.out.print("Dairenizini alanı  :" + alan);
     }
 }
